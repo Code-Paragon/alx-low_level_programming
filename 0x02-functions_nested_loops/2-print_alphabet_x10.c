6e0f142ce2773758b9bdacc0e41e6790153699a5
Adedeji <adedeji.mustapha.2018@gmail.com>
2023-06-20 07:46:36 -0700

    a function that prints 10 times the alphabet, in lowercase, followed by a new line.

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,15 +6,15 @@
  */
 void print_alphabet_x10(void)
 {
-        const char *str = "abcdefghijklmnopqrstuvwxyz";
-        int i, m;
+	const char *str = "abcdefghijklmnopqrstuvwxyz";
+	int i, m;
 
 	for (m = 0; m < 10; m++)
 	{
-        for (i = 0; str[i] != '\0'; i++)
+		for (i = 0; str[i] != '\0'; i++)
 	{
-                _putchar(str[i]);
+		_putchar(str[i]);
 	}
-        _putchar('\n');
+		_putchar('\n');
 }
 }
