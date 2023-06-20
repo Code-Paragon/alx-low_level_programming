098ea8ed90c16e82431135aef748ef998eae07f1
Adedeji <adedeji.mustapha.2018@gmail.com>
2023-06-20 07:13:28 -0700

    a program that prints _putchar, followed by a new line.

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -9,9 +9,10 @@ int main(void)
 {
 	const char *str = "_putchar";
 	int i;
-	for (i = 0; str[i] != '\0'; i++){
+
+	for (i = 0; str[i] != '\0'; i++)
 		_putchar(str[i]);
-	}
+
 	_putchar('\n');
 	return (0);
 }
