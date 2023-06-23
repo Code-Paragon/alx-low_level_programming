213b2d9e45b9a6ac7ca288cbd6eea06f56a7d154
Adedeji <adedeji.mustapha.2018@gmail.com>
2023-06-23 04:50:13 -0700

    making general indentaion corrections

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -11,22 +11,32 @@
 
 int largest_number(int a, int b, int c)
 {
-    int largest;
-
-    if (a < b && c < b)
-    {
-        largest = b;
-    }
-    else if (b < a && c < a)
-    {
-        largest = a;
-    }
-    else if (a < c && b < c)
-    {
-        largest = c;
-    }
-    else
-	largest = a;
-
-    return (largest);
+	int largest;
+	
+	if (a < b && c < b)
+	{
+		largest = b;
+	}
+	else if (b < a && c < a)
+	{
+		largest = a;
+	}
+	else if (a < c && b < c)
+	{
+		largest = c;
+	}
+	else if ((a == b) || (b == c) || (c == a))
+	{
+		if (c < a)
+			largest = a;
+		else
+			largest = c;
+		if (b < c)
+			largest = c;
+		else
+			largest = b;
+	}
+	else
+		largest = a;
+	return (largest);
 }
