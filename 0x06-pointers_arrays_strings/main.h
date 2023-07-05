6f6e15138e5c916022b2a83cfad94a53102aa6fe
Adedeji <adedeji.mustapha.2018@gmail.com>
2023-07-05 10:46:35 -0700

    a function that adds two numbers.

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -3,3 +3,5 @@ char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 int _putchar(char c);
 void print_number(int n);
+void rev_string(char *n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
