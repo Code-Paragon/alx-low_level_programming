9608ea65ec3ca9a4f7957ea056c83bd14d4d98d3
Adedeji <adedeji.mustapha.2018@gmail.com>
2023-07-06 13:08:39 -0700

    a function that searches a string for any of a set of bytes.

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+/**
+ * _strpbrk - function searches a str for any of a set of bytes
+ * @s: str
+ * @accept: str
+ *
+ * Return: char
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i,j,k = 0;
+	char *e;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				k++;
+				e = &s[i]
+				break;
+			}
+		}
+		if (k != 0)
+		{
+			break;
+		}
+	}
+	if (k != 0)
+	{
+		return (e);
+	}
+	else
+	{
+		return (NULL);
+	}
+}
