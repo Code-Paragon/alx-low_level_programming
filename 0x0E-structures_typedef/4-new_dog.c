8e7b77baa5ae1c70ab0008989fcc2009204296a7
Adedeji <adedeji.mustapha.2018@gmail.com>
2023-07-17 15:44:35 -0700

    new commit

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,19 +1,63 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
 /**
- * new_dog - a function that creates a new dog.
- * @owner: char *
- * @name: char*
- * @age: int
- *
- * Return: char *
+ * _strdup - malloc new str and copy *str into it
+ * @str: the string we copy
+ * Return: pointer to new string
  */
+char *_strdup(char *str)
+{
+	char *dest;
+	unsigned int i;
+	int count = 0;
+
+	if (str == NULL)
+		return (NULL);
+	for (i = 0; str[i]; i++)
+	{
+		count++;
+	}
+	count += 1;
+	dest = malloc(count * sizeof(char));
+	if (dest == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		dest[i] = str[i];
+	}
+	dest[i] = str[i];
+	return (dest);
+}
+/**
+  * new_dog - creates a new dog
+  * @name: name of dog
+  * @age: age of dog
+  * @owner: owner
+  * Return: pointer to new dog
+  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *newdog = malloc(sizeof(dog_t));
-	newdog->name = name;
-	newdog->age = age;
-	newdog->owner= owner;
-	return (newdog);
+	dog_t *puppy_dog;
+
+	puppy_dog = malloc(sizeof(dog_t));
+	if (puppy_dog == NULL)
+		return (NULL);
+	puppy_dog->name = _strdup(name);
+	if (puppy_dog->name == NULL)
+	{
+		free(puppy_dog);
+		return (NULL);
+	}
+	puppy_dog->owner = _strdup(owner);
+	if (puppy_dog->owner == NULL)
+	{
+		free(puppy_dog->name);
+		free(puppy_dog);
+		return (NULL);
+	}
+	puppy_dog->age = age;
+	return (puppy_dog);
 }
