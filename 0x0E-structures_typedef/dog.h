3083719b73c5ba40dbad4d4aff5ee65ff9e14ef2
Adedeji <adedeji.mustapha.2018@gmail.com>
2023-07-17 14:07:40 -0700

    a new type struct dog with the following elements:
    name, type = char *
    age, type = float
    owner, type = char *

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,13 @@
+/**
+ * struct dog - struct with members
+ * @name: First member
+ * @age: Second member
+ * @owner: third member
+ *
+ * Description: learning structs
+ */
+struct dog{
+	char *name;
+	float age;
+	char *owner;
+}
